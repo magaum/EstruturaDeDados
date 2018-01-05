09ebbb07ddda9fc94302e5ce8ef639f5c316cedd
weslei luiz <weslei_luiz22@hotmail.com>
2018-01-05 10:36:35 -0200

    Create No.java

---
+++ Hash/No.java
@@ -0,0 +1,10 @@
+package aula;
+
+public class No {
+	 public int chave, ocorrencia;
+	 public No proximo;
+	 @Override
+	 public String toString(){
+	 return "("+chave +":"+ ocorrencia+")";
+	 }
+}
